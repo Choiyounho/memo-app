9cdf5b042275abab6c0921538631ecf57a2812b4
Choiyounho <maxosa78@gmail.com>
2021-09-15 15:55:12 +0900

    [feat] : 메모 삭제 UI TEST

---
+++ app/src/androidTest/java/com/soten/memo/ui/DeleteMemoTest.kt
@@ -0,0 +1,120 @@
+package com.soten.memo.ui
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.soten.memo.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4ClassRunner::class)
+class DeleteMemoTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest2() {
+        val floatingActionButton = onView(
+            allOf(withId(R.id.memoEditButton),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_host_fragment_activity_main),
+                        0),
+                    1),
+                isDisplayed()))
+        floatingActionButton.perform(click())
+
+        val appCompatEditText = onView(
+            allOf(withId(R.id.editMemoTitleText),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                        0),
+                    0),
+                isDisplayed()))
+        appCompatEditText.perform(replaceText("qwe"), closeSoftKeyboard())
+
+        val appCompatEditText2 = onView(
+            allOf(withId(R.id.editMemoDescriptionText),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                        0),
+                    1),
+                isDisplayed()))
+        appCompatEditText2.perform(replaceText("asd"), closeSoftKeyboard())
+
+        val materialButton = onView(
+            allOf(withId(R.id.submitButton), withText("submit"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                        0),
+                    2),
+                isDisplayed()))
+        materialButton.perform(click())
+
+        val recyclerView = onView(
+            allOf(withId(R.id.memoListRecyclerView),
+                childAtPosition(
+                    withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                    0)))
+        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
+
+        val overflowMenuButton = onView(
+            allOf(withContentDescription("옵션 더보기"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.toolbar),
+                        1),
+                    1),
+                isDisplayed()))
+        overflowMenuButton.perform(click())
+
+        val materialTextView = onView(
+            allOf(withId(R.id.title), withText("delete"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.content),
+                        0),
+                    0),
+                isDisplayed()))
+        materialTextView.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int,
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
